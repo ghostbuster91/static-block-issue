d9d55a8418e1bef45fd7b7939204404610ac31fc
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-05-06 12:50:47 +0200

    Simplify more

--- src/com/example/MyIssue.java
+++ src/com/example/MyIssue.java
@@ -1,15 +1,14 @@
 package com.example;
 
-
 public sealed interface MyIssue {
-    String schema = MyIssue.A.schema;
+	String schema = MyIssue.A.schema;
 
-    //  commenting out below method fixes the issue
-    default <T> T accept(String var1) {
-            throw new RuntimeException("Unhandled union case");
-    }
+	// commenting out below method fixes the issue
+	default void accept(String var1) {
+	}
 
-    final class A implements MyIssue {
-        public static final String schema = new String("aa").trim(); //replacing this with inlined string also fixes the issue
-    }
+	final class A implements MyIssue {
+		public static final String schema = new String("aa").trim(); // replacing this with inlined string also fixes
+																		// the issue
+	}
 }
